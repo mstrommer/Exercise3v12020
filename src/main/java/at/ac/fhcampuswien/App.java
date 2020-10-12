dd9d4b5713d90f87c98e83c109b95cabd7cc9b26
Michael Strommer <>
2020-10-12 15:31:42 +0200

    Method headers inserted.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,11 +1,48 @@
 package at.ac.fhcampuswien;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
 
-    // Implement all methods as public static
+    // todo Task 1
+    public static void oneMonthCalendar(int days, int start) {
+        // code here...
+    }
 
-    // Also put all the method headers here, even if you don't implement the body of the
-    // method in order to compile the AppTest Class without errors.
+    // todo Task 2
+    public static long[] lcg(long seed) {
+        // code here...
+        return null;
+    }
+
+    // todo Task 3
+    public static void guessingGame(int numberToGuess) {
+        // code here...
+    }
+
+    public static int randomNumberBetweenOneAndHundred(){
+        // code here...
+        return -1;
+    }
+
+    // todo Task 4
+    public static boolean swapArrays(int[] array1, int[] array2) {
+        // code here...
+        return true;
+    }
+
+    // todo Task 5
+    public static String camelCase(String text) {
+        // code here...
+        return "";
+    }
+
+    // todo Task 6
+    public static int checkDigit(int [] code){
+        // code here...
+        return -1;
+    }
 
     public static void main(String[] args) {
         // test your method implementations here
