8459a5e589af210cd968a198824d503a8e91cd90
Michael Strommer <>
2020-10-12 16:31:03 +0200

    Comments added.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,47 +5,9 @@ import java.util.Scanner;
 
 public class App {
 
-    /*
-    // todo Task 1
-    public static void oneMonthCalendar(int days, int start) {
-        // code here...
-    }
-
-    // todo Task 2
-    public static long[] lcg(long seed) {
-        // code here...
-        return null;
-    }
-
-    // todo Task 3
-    public static void guessingGame(int numberToGuess) {
-        // code here...
-    }
-
-    public static int randomNumberBetweenOneAndHundred(){
-        // code here...
-        return -1;
-    }
-
-    // todo Task 4
-    public static boolean swapArrays(int[] array1, int[] array2) {
-        // code here...
-        return true;
-    }
-
-    // todo Task 5
-    public static String camelCase(String text) {
-        // code here...
-        return "";
-    }
-
-    // todo Task 6
-    public static int checkDigit(int [] code){
-        // code here...
-        return -1;
-    }
-
-*/
+    // Implement all methods as public static
+    // Also put all the method headers here, even if you don't implement the body of the
+    // method in order to compile the AppTest Class without errors.
 
     public static void main(String[] args) {
         // test your method implementations here
